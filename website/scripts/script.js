f243723ee9d018e5b610304afdb265ad242ed72b
James <james@jamesxu.dev>
2022-01-12 18:59:43 -0500

    fix unable to load data

--- website/scripts/script.js
+++ website/scripts/script.js
@@ -1,6 +1,6 @@
 
 (async () => {
-    var data = await fetch('../../server/dashboard.json').then(resp => resp.json())
+    var data = await fetch('https://raw.githubusercontent.com/jamxu88/mocovid/main/server/dashboard.json').then(resp => resp.json())
     const StackedChart = new StackedBarChart()
     StackedChart._setData(data)
     StackedChart._createChart()
