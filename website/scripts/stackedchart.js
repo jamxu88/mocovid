0e6bba378ec1dcd4dfb7e4b108ee3488e03c9b9b
James <james@jamesxu.dev>
2022-01-12 19:46:57 -0500

    filter demo

--- website/scripts/stackedchart.js
+++ website/scripts/stackedchart.js
@@ -1,5 +1,6 @@
 class StackedBarChart {
     constructor() {
+        this.filter = null
         this.options = {
             chart: {
                 type: 'bar',
@@ -49,9 +50,19 @@ class StackedBarChart {
         var studentCaseArray = []
         var schoolList = []
         data.forEach(school => {
-            staffCaseArray.push(school.staff_cases_over_10_days)
-            studentCaseArray.push(school.student_cases_over_10_days)
-            schoolList.push(school.school)
+            if(this.filter) {
+                console.log(this.filter)
+                if(school.school.toLowerCase().includes(this.filter)) {
+                    staffCaseArray.push(school.staff_cases_over_10_days)
+                    studentCaseArray.push(school.student_cases_over_10_days)
+                    schoolList.push(school.school)
+                }
+            }else {
+                staffCaseArray.push(school.staff_cases_over_10_days)
+                studentCaseArray.push(school.student_cases_over_10_days)
+                schoolList.push(school.school)
+            }
+            
         })
         arr = [{
             name: 'Staff Cases',
@@ -60,13 +71,19 @@ class StackedBarChart {
             name: 'Student Cases',
             data: studentCaseArray
         }]
-        this.options.chart.height = schoolList.length * 25
+        this.options.chart.height = schoolList.length * 25 < 300 ? 300 : schoolList.length * 25
         this.options.series = arr
         this.options.xaxis.categories = schoolList
+        //this.chart.updateOptions()
     }
     _createChart() {
         console.log(this.options)
+        this.chart = null;
         this.chart = new ApexCharts(document.querySelector("#data"), this.options);
         this.chart.render()
+        console.log('Chart Rendered')
+    }
+    _updateChart() {
+        this.chart.updateOptions(this.options)
     }
 }
