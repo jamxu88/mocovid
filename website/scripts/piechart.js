3062434262056f8b50275cbda7d2e81b1f454588
sswadkar <saraanshwadkar@gmail.com>
2022-01-18 19:17:28 -0500

    increase pie chart

--- website/scripts/piechart.js
+++ website/scripts/piechart.js
@@ -4,7 +4,7 @@ class PieChart {
         this.options = {
             series: [25, 15],
             chart: {
-            width: '50%',
+            width: '40%',
             type: 'pie',
             animations: {
               enabled: false
