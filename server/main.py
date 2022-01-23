f65b8af668e2f6e1fbe4d5b96c24720608db3a53
sswadkar <saraanshwadkar@gmail.com>
2022-01-23 02:39:01 -0500

    mk1t8

--- server/main.py
+++ server/main.py
@@ -27,6 +27,7 @@ for filename in days:
     if filename.endswith(".xlsx"):
         dates.append(filename[:len(filename) - 5])
         if filename.split(".")[1] == 'xlsx':
+            print(f"{directory}/{filename}")
             df = pd.read_excel(f"{directory}/{filename}", engine = "openpyxl")
         elif filename.split(".")[1] == 'xls':
             df = pd.read_excel(f"{directory}/{filename}")
